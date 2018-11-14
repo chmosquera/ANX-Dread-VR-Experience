d3f53e87bf4ec31d341ec816b1afbb6b30777454
Ross De Vito <rossdv@sbcglobal.net>
2018-11-13 17:37:05 -0800

    Merging with master

---
+++ Hallway/Assets/lightscript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lightscript : MonoBehaviour {
+
+    Light myLight;
+
+	// Use this for initialization
+	void Start () {
+        myLight = this.gameObject.GetComponent<Light>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        myLight.intensity += 0.1f;
+	}
+}

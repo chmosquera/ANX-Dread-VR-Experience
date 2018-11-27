358e787ba649a60583016bdfff730694a2935a43
Ross <rossdv@sbcglobal.net>
2018-11-27 00:23:23 -0800

    made a scene transition happen, tried to make it work with a hitbox but had no luck

---
+++ Main/Assets/Scene_Changer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene_Changer : MonoBehaviour {
+
+    public Animator animator;
+    public GameObject target;
+    private int levelToLoad;
+    TriggerBox box = new TriggerBox();
+
+    // Update is called once per frame
+    public void OnTriggerEnter(Collider other)
+    {
+        if (box.OnTrigger(other))
+        {
+            FadeToLevel(1);
+        }
+	}
+
+    public void FadeToLevel (int levelIndex)
+    {
+        levelToLoad = levelIndex;
+        animator.SetTrigger("Fade_Out");
+    }
+
+    public void OnFadeComplete ()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+}
---
+++ Main/Assets/TriggerBox.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerBox : MonoBehaviour
+{
+    public bool OnTrigger(Collider other)
+    {
+        Debug.Log("hey");
+        if (other.CompareTag("Camera"))
+        {
+            return true;
+            Debug.Log("hello");
+        }
+        return false;
+    }
+	
+}

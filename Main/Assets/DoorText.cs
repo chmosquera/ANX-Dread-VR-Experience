638a61817006b0351dcba0994341318d73f88c50
Heptnor <allenliu789@gmail.com>
2019-02-04 23:21:31 -0800

    Finally done with the hand scanner door hopefully

---
+++ Main/Assets/DoorText.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorText : MonoBehaviour {
+
+    public Rigidbody scanner;
+    public Vector3 move;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 v = scanner.position + move;
+        transform.position = v; 
+	}
+}

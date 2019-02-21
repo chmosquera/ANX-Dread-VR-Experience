632c23f2afb134a9639e0f505e14e6391aab6508
Bonita <bgalvanb@calpoly.edu>
2019-02-21 00:38:33 -0800

    Particle Effects in Progress

---
+++ Main/Assets/Scripts/ParticleEmitter.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleEmitter : MonoBehaviour
+{
+    public Sparks sparkPrefab;
+    public Air airPrefab;
+    private Sparks spark = null;
+    private Air airhiss = null;
+    public Quaternion rotation;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print("hit particle sphere");
+        print(other.gameObject.tag);
+        if (other.gameObject.CompareTag("Player"))
+        {
+            print("hello");
+            //Player in vacinity create steam or sparks on wal around them every couple seconds now  
+            InvokeRepeating("createEffect", Random.Range(0f, 1.0f), Random.Range(2.0f, 5.0f));
+
+        }
+    }
+
+    void createEffect()
+    {
+        print("yo");
+        if (Random.value < .5) //make steam 
+        {
+            spark = Instantiate(sparkPrefab);
+            print(transform.position);
+            spark.transform.localPosition = transform.position;
+            print(spark.transform.position);
+            //spark.transform.localRotation = rotation;
+        }
+        else {  // make air 
+            airhiss = Instantiate(airPrefab);
+            airhiss.transform.localPosition = transform.position;
+            print(airhiss.transform.position);
+            //airhiss.transform.localRotation = rotation;
+        }
+    }
+
+
+}
---
+++ Main/Assets/Scripts/sparkSound.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sparkSound : MonoBehaviour
+{
+
+    AudioSource fxSound;
+    public AudioClip backMusic;
+
+    // Use this for initialization
+    void Start()
+    {
+        fxSound = GetComponent<AudioSource>();
+        fxSound.Play();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}

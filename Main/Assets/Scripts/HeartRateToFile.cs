66041ed67d29df67121952115b01827ba1f325b2
Unknown <rossdv@sbcglobal.net>
2019-03-05 17:57:34 -0800

    heartrate printing script

---
+++ Main/Assets/Scripts/HeartRateToFile.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartRateToFile : MonoBehaviour
+{
+
+    public wrmhlRead hrMonitor;
+    private System.IO.StreamWriter file;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        string now = DateTime.Now.ToString("hh_mm_ss");
+        print(now);
+        string nameOfFile = "userStats" + now + ".txt";
+        file = new System.IO.StreamWriter(@"C:\Users\rdevito\Desktop\CIA\CIACapstone\CIACapstone\Main\heartRateStats\" + nameOfFile);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        file.WriteLine(Time.time.ToString() + "\t" + hrMonitor.heartrate + "\n");
+    }
+
+    void OnApplicationQuit()
+    {
+        file.Close();
+    }
+
+}

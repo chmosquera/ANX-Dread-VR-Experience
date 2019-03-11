00c1ba5e7149b15d7ecdd5c1721a8351e1c59c33
Unknown <rossdv@sbcglobal.net>
2019-03-10 22:48:33 -0700

    heart rate printing

---
+++ Main/Assets/Scripts/HeartRateManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartRateManager : MonoBehaviour
+{
+
+    public GameManager gm;
+    public HeartRateToFile crash;
+    public HeartRateToFile puzzle;
+    public HeartRateToFile maze;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //starts with crash active 
+        if(gm.GetGameState() == GameState.Recover && crash.gameObject.activeSelf)
+        {
+            crash.gameObject.SetActive(false);
+            puzzle.gameObject.SetActive(true);
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if(other.gameObject.CompareTag("Player") || other.transform.parent.gameObject.CompareTag("Player"))
+        {
+            print("HELLO");
+            puzzle.gameObject.SetActive(false);
+            maze.gameObject.SetActive(true);
+        }
+    }
+
+
+}
--- Main/Assets/Scripts/HeartRateToFile.cs
+++ Main/Assets/Scripts/HeartRateToFile.cs
@@ -8,20 +8,33 @@ public class HeartRateToFile : MonoBehaviour
 
     public wrmhlRead hrMonitor;
     private System.IO.StreamWriter file;
+    public string location;
 
     // Start is called before the first frame update
     void Start()
     {
         string now = DateTime.Now.ToString("hh_mm_ss");
         //print(now);
-        string nameOfFile = "userStats" + now + ".txt";
+        string nameOfFile = "userStats" + location + now + ".txt";
         file = new System.IO.StreamWriter(@"C:\Users\rdevito\Desktop\CIA\CIACapstone\CIACapstone\Main\heartRateStats\" + nameOfFile);
+ 
     }
 
     // Update is called once per frame
     void Update()
     {
-        file.WriteLine(Time.time.ToString() + "\t" + hrMonitor.heartrate + "\n");
+        string rate = hrMonitor.heartrate;
+        print("heelo");
+        //if (isNumeric(rate))
+        //{
+            file.WriteLine(Time.time.ToString() + "\t" + rate + "\n");
+        //}
+    }
+
+    public bool isNumeric(string value)
+    {
+        int test;
+        return int.TryParse(value, out test);
     }
 
     void OnApplicationQuit()

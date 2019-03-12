8880d8a4ba5e1cc4d090f10bc3bd526a488c288e
Chanelle <ckaith97@yahoo.com>
2019-03-12 14:31:27 -0700

    Changed gravity at end scene. Fixed sprites.

--- Main/Assets/Scripts/HeartbeatHallwayManager.cs
+++ Main/Assets/Scripts/HeartbeatHallwayManager.cs
@@ -14,6 +14,11 @@ public class HeartbeatHallwayManager : MonoBehaviour
     public AudioClip endMusic;
     public AudioClip hallwayMusic;
 
+    public GameObject VRTK_SDKManager;
+    public GameObject SteamVR;
+    public GameObject VRSimulator;
+    public Rigidbody playerRB;
+
     [Header("General Settings")]
     public Hallway2State state;
 
@@ -21,12 +26,22 @@ public class HeartbeatHallwayManager : MonoBehaviour
 
     void Start()
     {
+        //print("--------------------" + VRTK_SDKManager.GetComponentInChildren<Hallway_Zoom>().name);
+        //player = VRTK_SDKManager.GetComponentsInChildren(typeof(Hallway_Zoom), false)[0].GetComponent<Rigidbody>();
+        
         SetState(new WalkingThroughState(this));
     }
 
     void Update()
     {
-        currentState.Tick();
+        if (VRSimulator.activeSelf) {
+            playerRB = VRSimulator.GetComponentInChildren<Hallway_Zoom>().GetComponent<Rigidbody>();
+            print ("--------" + playerRB.name);
+        } else if (SteamVR.activeSelf) {
+            playerRB = SteamVR.GetComponentInChildren<Hallway_Zoom>().GetComponent<Rigidbody>();
+            print ("--------" + playerRB.name);
+        }        
+        if (currentState != null) currentState.Tick();
     }
 
     public void SetState(State state)

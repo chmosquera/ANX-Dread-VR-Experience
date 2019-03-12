a81bc31ea4c6d8795b7055e2ae278fcc57f14ae8
chanelle <cmosquer@calpoly.edu>
2019-03-12 14:55:51 -0700

    triggercake

---
+++ Main/Assets/TriggerCake.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerCake : MonoBehaviour
+{
+    public void OnTriggerEnter(Collider other)
+    {
+        print("entered");
+        print(other.transform.root.tag);
+        if (other.transform.root.tag == "Player")
+        {
+            print("collided");
+            Destroy(this.gameObject);
+        }
+    }
+    // Update is called once per frame
+   
+}

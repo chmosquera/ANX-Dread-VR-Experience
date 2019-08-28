431ffdc234113a65082956d949ae33c6f3ecf98e
Ellen Liu <allenliu789@gmail.com>
2019-08-28 14:46:31 -0700

    Update HeartBeat Script

--- Main/Assets/Scripts/HeartBeatDisplay.cs
+++ Main/Assets/Scripts/HeartBeatDisplay.cs
@@ -6,17 +6,59 @@ using UnityEngine.UI;
 
 public class HeartBeatDisplay : MonoBehaviour {
 
+    public GameManager gm;
     public Text hb;
-    public wrmhlRead heartrateMonitor;
+    //public wrmhlRead heartrateMonitor;
+    public int heartBeat;
+    public int hbCounter;
 
+    private void Start()
+    {
+        heartBeat = 70;
+        hb.text = heartBeat.ToString();
+        hbCounter = 5000;
+    }
     private void Update()
     {
-            UpdateHeartBeat();
+        UpdateHeartBeat();
     }
 
     public void UpdateHeartBeat()
     {
 
-        hb.text = heartrateMonitor.heartrate;
+        //hb.text = heartrateMonitor.heartrate;
+        // before crash, start at 70 
+        if (gm.GetGameState() == GameState.Intro)
+        {
+            if (heartBeat < 76 && hbCounter == 0)
+            {
+                heartBeat++;
+                hbCounter = 5000;
+            }
+                
+        }
+        // at crash, jump to 100
+        else if (gm.GetGameState() == GameState.Crash)
+        {
+            print(hbCounter);
+            if (heartBeat < 100)
+                heartBeat = 100;
+            else if (heartBeat < 130 && hbCounter == 0)
+            {
+                heartBeat++;
+                hbCounter = 3000;
+            }
+        }
+        // in maze peak at 150
+        else if (gm.GetGameState() == GameState.Recover)
+        {
+            // heartBeat = 150;
+            if (heartBeat < 150)
+                heartBeat+= (int)Time.deltaTime;
+        }
+        hb.text = heartBeat.ToString();
+        hbCounter--;
     }
+
+    
 }
